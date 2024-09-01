4693e5f656b58645d6bd2a7ae9b63a9171ec564f
bilgejava6 <bilgejava6@gmail.com>
2024-09-01 16:25:58 +0300

    Initial commit

---
+++ src/main/java/com/muhammet/stoktakipotomasyon/StokTakipOtomasyonApplication.java
@@ -0,0 +1,13 @@
+package com.muhammet.stoktakipotomasyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokTakipOtomasyonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StokTakipOtomasyonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muhammet/stoktakipotomasyon/StokTakipOtomasyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhammet.stoktakipotomasyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokTakipOtomasyonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
