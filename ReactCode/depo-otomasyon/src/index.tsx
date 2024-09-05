8d592fe04b43e4f7defed8dffbc81b2c1d5f6a37
bilgejava6 <bilgejava6@gmail.com>
2024-09-05 20:04:12 +0300

    React kodlamaları 1. kısım, Login,Register, Home

--- ReactCode/depo-otomasyon/src/index.tsx
+++ ReactCode/depo-otomasyon/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Routerpages from "./routerpages";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<h1>Uygulama</h1>);
+root.render(<Routerpages />);
---
+++ ReactCode/depo-otomasyon/src/routerpages.tsx
@@ -0,0 +1,6 @@
+
+function Routerpages(){
+    return(<h1>Router Page</h1>);
+}
+
+export default Routerpages;
